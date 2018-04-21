70e548440c3ca205096062578d9b59f84e9d80e2
Tomas <slidertom@gmail.com>
2018-04-22 02:59:45 +0300

    source add

---
+++ ReadMe.txt
@@ -0,0 +1,16 @@
+========================================================================
+       CONSOLE APPLICATION : AppClose
+========================================================================
+
+AppClose direct usage examples:
+  AppClose.exe 1234
+  AppClose.exe c:\path\app.pid
+
+where 
+  1234 - process pid
+  c:\path\app.pid - contains 1234
+
+
+See for more details:
+  http://bugzilla.matrixlt.local/show_bug.cgi?id=19433
+  #19433 - P20: Apache server startup as service research
---
+++ StdAfx.h
@@ -0,0 +1,15 @@
+#if !defined(AFX_STDAFX_H__050B3017_8126_4395_89D0_A5BF1C62BCBA__INCLUDED_)
+#define AFX_STDAFX_H__050B3017_8126_4395_89D0_A5BF1C62BCBA__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+//'function' : This function or variable may be unsafe.
+#pragma warning(disable: 4996)
+
+#endif // !defined(AFX_STDAFX_H__050B3017_8126_4395_89D0_A5BF1C62BCBA__INCLUDED_)
